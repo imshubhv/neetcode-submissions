5d899aa72f9c746ab52384c48f9d5b79dff8c1f1
Shubham Verma <34945955+imshubhv@users.noreply.github.com>
2026-06-25 11:31:05 +0530

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.go
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+    if list1 == nil {
+        return list2
+    } else if list2 == nil {
+        return list1
+    }
+
+    dummy := &ListNode{}
+    current := dummy
+
+    for list1 != nil && list2 != nil {
+        if list1.Val <= list2.Val {
+            current.Next = list1
+            list1 = list1.Next
+        } else {
+            current.Next = list2
+            list2 = list2.Next
+        }
+        current = current.Next
+    }
+
+    // ✅ attach remaining
+    if list1 != nil {
+        current.Next = list1
+    } else {
+        current.Next = list2
+    }
+
+    return dummy.Next
+}
