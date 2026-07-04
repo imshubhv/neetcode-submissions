5eab8193268903d3c771590ab7b249b217caeade
Shubham Verma <34945955+imshubhv@users.noreply.github.com>
2026-07-04 16:54:16 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.go
@@ -0,0 +1,31 @@
+func productExceptSelf(nums []int) []int {
+	hasZero, hasTwoZeros := false, false
+	var product int = 1
+	output := make( []int, len(nums))
+	for _, value := range nums{
+		if value == 0 {
+			if hasZero == true {
+				hasTwoZeros = true
+				break
+			}
+			hasZero = true
+		} else {
+			product = product * value
+		}
+	}
+	if hasTwoZeros{
+		return output
+	}else if !hasZero{
+		for index, value:= range nums {
+			output[index] = product / value
+		}
+	} else {
+		for index, value:= range nums {
+			if (value == 0) {
+				output[index] = product
+				break
+			}
+		}
+	}	
+	return output
+}
