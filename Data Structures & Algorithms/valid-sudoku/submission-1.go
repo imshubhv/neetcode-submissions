c59d8ab50dd8e3757b079557abb7fde77c125eec
Shubham Verma <34945955+imshubhv@users.noreply.github.com>
2026-07-05 12:23:05 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.go
@@ -0,0 +1,45 @@
+func isValidSudoku(board [][]byte) bool {
+    row:= 0
+    for row < 9 {
+        numbers:= map[byte]bool{}
+        for i:=0; i<9; i++ {
+            val:= board[row][i]
+            if numbers[val]{
+                return false
+            } else if val != '.'{
+                numbers[val] = true
+            }
+        }
+        row++
+    }
+
+    column:= 0
+    for column < 9 {
+        numbers:= map[byte]bool{}
+        for i:=0; i<9; i++ {
+            val:= board[i][column]
+            if numbers[val]{
+                return false
+            } else if val != '.'{
+                numbers[val] = true
+            }
+        }
+        column++
+    }
+    for boxRow:= 0; boxRow<9; boxRow+=3 {
+        for boxCol:= 0; boxCol<9; boxCol+=3 {
+            numbers:= map[byte]bool{}
+            for i:= boxRow ; i< boxRow+3 ; i++ {
+                for j:=boxCol; j< boxCol+3 ; j++ {
+                    val:= board[i][j]
+                    if numbers[val]{
+                        return false
+                    } else if val != '.'{
+                        numbers[val] = true
+                    }
+                }
+            }
+        }
+    }
+    return true
+}
